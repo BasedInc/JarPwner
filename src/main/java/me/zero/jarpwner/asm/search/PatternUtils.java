8b79a2c51f169b77d1711d6d9cb62cb70eb8be65
Brady <zeromemesdev@gmail.com>
2020-03-26 13:04:31 -0500

    1 method

--- src/main/java/me/zero/jarpwner/asm/search/PatternUtils.java
+++ src/main/java/me/zero/jarpwner/asm/search/PatternUtils.java
@@ -13,28 +13,40 @@ public final class PatternUtils {
 
     private PatternUtils() {}
 
-    public static InsnPredicate literal(int opcode) {
-        return new LiteralPredicate(opcode);
+    public static InsnPredicate type(int... types) {
+        switch (types.length) {
+            case 0: {
+                throw new IllegalArgumentException("At least one type must be provided");
+            }
+            case 1: {
+                return new TypePredicate(types[0]);
+            }
+            default: {
+                return Arrays.stream(types).boxed().reduce(
+                    type -> false,
+                    (check, type) -> check.or(type(type)),
+                    InsnPredicate::or
+                );
+            }
+        }
     }
 
-    public static InsnPredicate type(int type) {
-        return new TypePredicate(type);
-    }
-
-    public static InsnPredicate anyLiteral(int... opcodes) {
-        return Arrays.stream(opcodes).boxed().reduce(
-            opcode -> false,
-            (check, opcode) -> check.or(literal(opcode)),
-            InsnPredicate::or
-        );
-    }
-
-    public static InsnPredicate anyType(int... types) {
-        return Arrays.stream(types).boxed().reduce(
-            opcode -> false,
-            (check, type) -> check.or(type(type)),
-            InsnPredicate::or
-        );
+    public static InsnPredicate literal(int... opcodes) {
+        switch (opcodes.length) {
+            case 0: {
+                throw new IllegalArgumentException("At least one opcode must be provided");
+            }
+            case 1: {
+                return new LiteralPredicate(opcodes[0]);
+            }
+            default: {
+                return Arrays.stream(opcodes).boxed().reduce(
+                    opcode -> false,
+                    (check, opcode) -> check.or(literal(opcode)),
+                    InsnPredicate::or
+                );
+            }
+        }
     }
 
     public static InsnPredicate any() {
